9bdc4e99c81dc3b9e6f67f4386821cd08fa71cf7
DJTwoTone <34737313+DJTwoTone@users.noreply.github.com>
2021-04-20 19:02:58 +0900

    change db setting

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ const { Client } = require('pg');
 const { DB_URI } = require('./config');
 
 const db = new Client({
-    connectionString: DB_URI
+    connectionString: DB_URI,   
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 
